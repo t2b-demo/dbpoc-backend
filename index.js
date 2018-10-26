e7409c1e095785fd0464c6d4ee2b1b977f383fe0
Bernard Adanlessossi <bernard.adanlessossi@t2b.ch>
2018-10-26 14:51:19 +0200

    Mongodb url fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const TortoiseDB = require('./dist/tortoiseDB.min.js');
 const db = new TortoiseDB({
   name: 'demo',
   port: 3000,
-  mongoURI: 'mongodb://testuser:Passw0rd@ubuntu@3.120.178.19:27017/tortoisedb-demo',
+  mongoURI: 'mongodb://testuser:Passw0rd@3.120.178.19/tortoisedb-demo',
   batchLimit: 1000
 });
 
