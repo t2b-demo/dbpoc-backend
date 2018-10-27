336ebb09972371c8d49c98b500551e86412f2af8
Bernard Adanlessossi <bernard.adanlessossi@t2b.ch>
2018-10-27 10:50:14 +0200

    Some minor bug fixes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const TortoiseDB = require('./dist/tortoiseDB.min.js');
 const db = new TortoiseDB({
   name: 'demo',
   port: 3000,
-  mongoURI: 'mongodb://testuser:Passw0rd@35.159.30.70:27017/tortoisedb-demo',
+  mongoURI: 'mongodb://testuser:Passw0rd@35.159.30.70/tortoisedb-demo',
   batchLimit: 1000
 });
 
